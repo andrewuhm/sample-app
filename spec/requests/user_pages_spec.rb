9c2c7c60d655f8251d6e00aeaf19fb59f108476a
Andrew Uhm <andrewuhm@gmail.com>
2013-07-10 16:51:51 -0700

    Finish exercise 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,9 +23,19 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      before { click_button submit }
       it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        expect { not_to change(User, :count) }
       end
+
+      describe "after submission" do
+        it { should have_title('Sign up') }
+        it { should have_content('error', text: 'Password') }
+        it { should have_content('error', text: 'Password can\'t be blank') }
+        it { should have_content('error', text: 'Password is too short') }
+        it { should have_content('error', text: 'Email is invalid') }
+      end
+
     end
 
     describe "with valid information" do
@@ -38,6 +48,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
